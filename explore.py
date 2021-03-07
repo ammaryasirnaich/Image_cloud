923049b93dd4ed849ce0809e7dc9a2678e7fc978
Ammar Yasir Naich <ammar.naich@gmail.com>
2021-03-07 13:54:41 +0000

    1- cloudpoint generated using open3d 2- util package created 3- cloudpoint using opencv also added

--- explore.py
+++ explore.py
@@ -1,121 +1,7 @@
-import cv2
-import numpy as np
-import matplotlib.pyplot as plt
+#from utils import *
 
-import pykitti
-import os
+import utils
 import open3d as o3d
-
-# print(os.getcwd())
-
-
-# load dataset with pykitti
-# Load the data. Optionally, specify the frame range to load.
-def load_dataset(basedir, date, drive, calibrated=False, frame_range=None):
-    """
-    Loads the dataset with `date` and `drive`.
-    
-    Parameters
-    ----------
-    date        : Dataset creation date.
-    drive       : Dataset drive.
-    calibrated  : Flag indicating if we need to parse calibration data. Defaults to `False`.
-    frame_range : Range of frames. Defaults to `None`.
-
-    Returns
-    -------
-    Loaded dataset of type `raw`.
-    """
-    #dataset = pykitti.raw(basedir, date, drive,frames=range(0, 20, 5))
-    dataset = pykitti.raw(basedir, date, drive)
-   
-    
-    # Load the data
-    if calibrated:
-        dataset._load_calib()  # Calibration data are accessible as named tuples
-
-    np.set_printoptions(precision=4, suppress=True)
-    print('\nDrive: ' + str(dataset.drive))
-    print('\nFrame range: ' + str(dataset.frames))
-
-    if calibrated:
-        print('\nIMU-to-Velodyne transformation:\n' + str(dataset.calib.T_velo_imu))
-        print('\nGray stereo pair baseline [m]: ' + str(dataset.calib.b_gray))
-        print('\nRGB stereo pair baseline [m]: ' + str(dataset.calib.b_rgb))
-
-    return dataset
-
-
-# set stereo Settings
-def stereo_setting_disparity():
-        
-    ### START CODE HERE ###
-    
-    # Parameters
-    num_disparities = 6*16
-    block_size = 11
-    
-    min_disparity = 0
-    window_size = 6
-
-    # Stereo SGBM matcher
-    stereo_matcher_SGBM = cv2.StereoSGBM_create(
-        minDisparity=min_disparity,
-        numDisparities=num_disparities,
-        blockSize=block_size,
-        P1=8 * 3 * window_size ** 2,
-        P2=32 * 3 * window_size ** 2,
-        mode=cv2.STEREO_SGBM_MODE_SGBM_3WAY
-    )
-
-    return stereo_matcher_SGBM
-
-
-def compute_disparity_map(img_left, img_right, stereo_matcher_SGBM):
-    # Compute the left disparity map
-    disp_left = stereo_matcher_SGBM.compute(img_left, img_right).astype(np.float32)/16   
-    return disp_left
-
-
-def depth_from_disparity(disparity,k_,t_):
-    
-    # Get the focal length from the K matrix
-    f = k_[0, 0]
-    
-    print("focal length of kitti lense:{}".format(f))
-    # Get the distance between the cameras from the t matrices (baseline)
-    #b = t_left[1] - t_right[1]
-    
-    b = 0.54  # Taken from the kitti website, looking into the sensor suite setup its 0.54 meters
-
-    
-    
-    # Replace all instances of 0 and -1 disparity with a small minimum value (to avoid div by 0 or negatives)
-    disparity[disparity == 0] = 0.1
-    disparity[disparity == -1] = 0.1
-
-    # Initialize the depth map to match the size of the disparity map
-    depth_map = np.ones(disparity.shape, np.single)
-
-    # Calculate the depths 
-    depth_map[:] = f * b / disparity[:]
-    
-    return depth_map
-
-
-def decompose_projection_matrix(P_matrix):
-    
-    ### START CODE HERE ###
-    
-    k, r, t, _, _, _, _ = cv2.decomposeProjectionMatrix(P_matrix)
-    t = t / t[3]
-    
-    ### END CODE HERE ###
-    
-    return k, r, t
-
-
-
 
 
 
@@ -125,119 +11,35 @@ def main():
 
     #Path for Kitti Dataset
 
-    basedir = '/home/'
+    basedir = '/media/ammar/eecs_ammar/Kitti'
+
+ 
     # Specify the dataset to load
     date = '2011_09_26'
     drive = '0009'
-    dataset = load_dataset(basedir, date,drive,True)
+    dataset = utils.load_dataset(basedir, date,drive,True)
+    previous_pcd = o3d.cuda.pybind.geometry.PointCloud
+    out_fn = 'from_OpenCV_method.ply'
 
-
-    #Compute the disparity map
-    # Calling the settings for stereo disparity matcher
-    stereo_matcher_SGBM = stereo_setting_disparity()
-    #computer the disparity using stereo images
-
-    frame =10
-    img_left_rgb = np.array(dataset.get_cam2(frame))
-    img_right_rgb = np.array(dataset.get_cam3(frame))
-    #print(img_left_rgb.shape)
-
-    # converting RGB to Grey images for computing disparity map
-    img_left_grey = cv2.cvtColor(img_left_rgb , cv2.COLOR_RGB2GRAY)
-    img_right_grey = cv2.cvtColor(img_right_rgb , cv2.COLOR_RGB2GRAY)
-
-    print(img_left_grey.shape)
-
-    disparaity_map = compute_disparity_map(img_left_grey, img_right_grey,stereo_matcher_SGBM)
-    
-    
-        # Read the calibration 
-    # As the P matrix is the combination intrinsic parameters 𝐾  and the extrinsic rotation 𝑅,
-    # and translation t as follows:
-    
-    P_matrix=  dataset.calib.P_rect_00
-
-    # Decompose each matrix
-    k_, r_, t_ = decompose_projection_matrix(P_matrix)
-
-    # Display the matrices
-    # print("P_matrix")
-    # print(P_matrix)
-
-
-    # print("decompouse matrics")
-    # print("k_ \n", k_)
-    # print("\nr_ \n", r_)
-    # print("\nt_ \n", t_)
-    
-    # Get the depth map by calling the above function
-    depth_map = depth_from_disparity(disparaity_map, k_, t_)
-
-    print(type(img_left_rgb))
-    print(type(depth_map))
-
-    color_raw = o3d.geometry.Image(img_left_rgb)
-    depth_raw = o3d.geometry.Image(depth_map)
-
-    # color_raw = o3d.io.read_image(img_left_rgb)
-    # depth_raw = o3d.io.read_image(depth_map)
-    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
-    # print(rgbd_image)
-
-    # plt.subplot(1, 2, 1)
-    # plt.title('Kitti grayscale image')
-    # plt.imshow(rgbd_image.color)
-    # plt.subplot(1, 2, 2)
-    # plt.title('kitti depth image')
-    # plt.imshow(rgbd_image.depth)
-    # plt.show()
-    
-    cam = o3d.camera.PinholeCameraIntrinsic()
-    cam.intrinsic_matrix =  k_
-    pcd = o3d.geometry.PointCloud.create_from_rgbd_image( rgbd_image, o3d.camera.PinholeCameraIntrinsic(cam) )
-    # Flip it, otherwise the pointcloud will be upside down
-    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
-
-    # o3d.visualization.draw_geometries([pcd], zoom=0.5)
-    o3d.visualization.draw_geometries([pcd])
-
+    for frameNumber in range(1,2,1):
+        print(frameNumber)  
+        pcd = utils.pointcloud_from_StereoImage_kitti(frameNumber,dataset)
+        #previous_pcd+=pcd
+        #print(type(pcd))
+        #o3d.io.write_point_cloud("from_open3d_method.ply", pcd)
 
         
+        #pcd , pcd_color = utils.cv_pointcloud_from_stereo(dataset,frameNumber)
+        #utils.write_ply(out_fn, pcd, pcd_color)
+        #print('%s saved' % out_fn)
 
+
+    #o3d.io.write_point_cloud("copy_of_fragment.ply", previous_pcd)
 
 
 if __name__ == '__main__':
-    # main()
-    # generate some neat n times 3 matrix using a variant of sync function
-    x = np.linspace(-3, 3, 401)
-    mesh_x, mesh_y = np.meshgrid(x, x)
-    z = np.sinc((np.power(mesh_x, 2) + np.power(mesh_y, 2)))
-    z_norm = (z - z.min()) / (z.max() - z.min())
-    xyz = np.zeros((np.size(mesh_x), 3))
-    xyz[:, 0] = np.reshape(mesh_x, -1)
-    xyz[:, 1] = np.reshape(mesh_y, -1)
-    xyz[:, 2] = np.reshape(z_norm, -1)
-    print('xyz')
-    print(xyz)
+    main()
 
-    # Pass xyz to Open3D.o3d.geometry.PointCloud and visualize
-    pcd = o3d.geometry.PointCloud()
-    pcd.points = o3d.utility.Vector3dVector(xyz)
-    o3d.io.write_point_cloud("../../TestData/sync.ply", pcd)
-
-    # Load saved point cloud and visualize it
-    pcd_load = o3d.io.read_point_cloud("../../TestData/sync.ply")
-    o3d.visualization.draw_geometries([pcd_load])
-
-    # convert Open3D.o3d.geometry.PointCloud to numpy array
-    xyz_load = np.asarray(pcd_load.points)
-    print('xyz_load')
-    print(xyz_load)
-
-    # save z_norm as an image (change [0,1] range to [0,255] range with uint8 type)
-    img = o3d.geometry.Image((z_norm * 255).astype(np.uint8))
-    o3d.io.write_image("../../TestData/sync.png", img)
-    o3d.visualization.draw_geometries([img])
     
 
 
--- utils.py
+++ utils.py
@@ -4,11 +4,14 @@ import matplotlib.pyplot as plt
 
 import pykitti
 import os
+import open3d as o3d
+
+# print(os.getcwd())
 
 
 # load dataset with pykitti
 # Load the data. Optionally, specify the frame range to load.
-def load_dataset(date, drive, calibrated=False, frame_range=None):
+def load_dataset(basedir, date, drive, calibrated=False, frame_range=None):
     """
     Loads the dataset with `date` and `drive`.
     
@@ -32,16 +35,15 @@ def load_dataset(date, drive, calibrated=False, frame_range=None):
         dataset._load_calib()  # Calibration data are accessible as named tuples
 
     np.set_printoptions(precision=4, suppress=True)
-    print('\nDrive: ' + str(dataset.drive))
-    print('\nFrame range: ' + str(dataset.frames))
+    #print('\nDrive: ' + str(dataset.drive))
+    #print('\nFrame range: ' + str(dataset.frames))
 
-    if calibrated:
-        print('\nIMU-to-Velodyne transformation:\n' + str(dataset.calib.T_velo_imu))
-        print('\nGray stereo pair baseline [m]: ' + str(dataset.calib.b_gray))
-        print('\nRGB stereo pair baseline [m]: ' + str(dataset.calib.b_rgb))
+    #if calibrated:
+    #    print('\nIMU-to-Velodyne transformation:\n' + str(dataset.calib.T_velo_imu))
+    #    print('\nGray stereo pair baseline [m]: ' + str(dataset.calib.b_gray))
+    #    print('\nRGB stereo pair baseline [m]: ' + str(dataset.calib.b_rgb))
 
     return dataset
-
 
 
 # set stereo Settings
@@ -71,21 +73,31 @@ def stereo_setting_disparity():
 
 def compute_disparity_map(img_left, img_right, stereo_matcher_SGBM):
     # Compute the left disparity map
-    disp_left = stereo_matcher_SGBM.compute(img_left, img_right).astype(np.float32)/16
-    ### END CODE HERE ###
-    
+    disp_left = stereo_matcher_SGBM.compute(img_left, img_right).astype(np.float32)/16    
     return disp_left
 
-def depth_from_disparity(disparity,k_,t_):
-        ### START CODE HERE ###
+
+def depth_from_disparity(disparity,k_=None,t_=None,focal_length=None):
     
     # Get the focal length from the K matrix
-    f = k_[0, 0]
 
+
+    #f = k_[0, 0]
+
+
+    if k_.all() != None:
+        f = k_[0, 0]
+    
+    if focal_length != None:
+        f = focal_length
+
+    #print("focal length of kitti lense:{}".format(f))
     # Get the distance between the cameras from the t matrices (baseline)
     #b = t_left[1] - t_right[1]
-    b = 0.54  # base = 0.54 meters, taken from the kitti website, looking into the sensor suite setup
+    
+    b = 0.54  # Taken from the kitti website, looking into the sensor suite setup its 0.54 meters
 
+    
     # Replace all instances of 0 and -1 disparity with a small minimum value (to avoid div by 0 or negatives)
     disparity[disparity == 0] = 0.1
     disparity[disparity == -1] = 0.1
@@ -94,39 +106,264 @@ def depth_from_disparity(disparity,k_,t_):
     depth_map = np.ones(disparity.shape, np.single)
 
     # Calculate the depths 
-    depth_map[:] = f * b / disp_left[:]
+    depth_map[:] = f * b / disparity[:]
     
-    ### END CODE HERE ###
     return depth_map
 
 
 def decompose_projection_matrix(P_matrix):
-    
-    ### START CODE HERE ###
-    
     k, r, t, _, _, _, _ = cv2.decomposeProjectionMatrix(P_matrix)
-    t = t / t[3]
-    
-    ### END CODE HERE ###
-    
+    #t = t / t[3]
     return k, r, t
 
+def pointcloud_from_StereoImage_kitti(frameNumber,dataset):
+    #Compute the disparity map
+    # Calling the settings for stereo disparity matcher
+    stereo_matcher_SGBM = stereo_setting_disparity()
+    #computer the disparity using stereo images
+
+
+    #img_left_rgb = np.array(cv2.imread("left.png"))
+    #img_right_rgb = np.array(cv2.imread("right.png"))
+
+    img_left_rgb = np.array(dataset.get_cam2(frameNumber))
+    img_right_rgb = np.array(dataset.get_cam3(frameNumber))
+    #print(img_left_rgb.shape)
+
+    # converting RGB to Grey images for computing disparity map
+    img_left_grey = cv2.cvtColor(img_left_rgb , cv2.COLOR_RGB2GRAY)
+    img_right_grey = cv2.cvtColor(img_right_rgb , cv2.COLOR_RGB2GRAY)
+
+    #print(img_left_grey.shape)
+
+    disparaity_map = compute_disparity_map(img_left_grey, img_right_grey,stereo_matcher_SGBM)
+    
+    # Read the calibration 
+    # As the P matrix is the combination intrinsic parameters 𝐾  and the extrinsic rotation 𝑅,
+    # and translation t as follows:   
+    P_matrix=  dataset.calib.P_rect_00
+    #camera_extrinsic
+
+    # Decompose each matrix
+    #k_method, r_, t_ = decompose_projection_matrix(P_matrix)
+    
+    camera_intrinsic_para, camera_rotation_para, camera_trans_para,_,_,_,_ = cv2.decomposeProjectionMatrix(P_matrix)
+
+    # Display the matrices
+    # print("camera_intrinsic_para")
+    # print(camera_intrinsic_para)
+
+    # print("decompouse matrics")
+    # print("k_ \n", k_)
+    # print("\nr_ \n", r_)
+    # print("\nt_ \n", t_)
+    
+    # Get the depth map by calling the above function
+    depth_map = depth_from_disparity(disparaity_map, camera_intrinsic_para, camera_trans_para)
+
+
+    color_raw = o3d.geometry.Image(img_left_rgb)
+    depth_raw = o3d.geometry.Image(depth_map)
+
+    # ** Note add Externsic parameters below
+    rgbd_image = o3d.geometry.RGBDImage.create_from_color_and_depth(color_raw, depth_raw)
+
+    print("RGBD information:")
+    print(rgbd_image)
+
+    cam = o3d.camera.PinholeCameraIntrinsic()
+    cam.intrinsic_matrix =  camera_intrinsic_para
+    
+
+    #cam = o3d.camera.PinholeCameraParameters()
+    #cam.intrinsic = camera_intrinsic_para
+    #cam.extrinsic = np.array([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]])
+    extrinsic_para = np.array([[1., 0., 0., 0.], [0., -1., 0., 0.], [0., 0., 0., 0.], [0., 0., 1., 0.]])
+
+
+    #print("Camera Intrinsic parameters")
+    #print(cam.intrinsic_matrix)
+
+    #print("CV demonposition method")
+    #print(camera_trans_para)
+
+    
+    pcd = o3d.geometry.PointCloud.create_from_rgbd_image( rgbd_image, o3d.camera.PinholeCameraIntrinsic(cam))
+    
+    #pcd = o3d.geometry.PointCloud.create_from_rgbd_image( rgbd_image, cam.intrinsic,cam.extrinsic )
+    
+
+    # Flip it, otherwise the pointcloud will be upside down
+    pcd.transform([[1, 0, 0, 0], [0, -1, 0, 0], [0, 0, -1, 0], [0, 0, 0, 1]])
+
+    #print(pcd)
+
+
+    #o3d.visualization.draw_geometries([pcd])
+
+    visualize(img_left_rgb,img_right_rgb,disparaity_map,depth_map,rgbd_image,True)
+    
 
 
 
-def visualize(img_left_rgb,img_right_rgb,disparaity_map,depth_map):
-    # Display some data
-    f, ax = plt.subplots(2, 2, figsize=(15, 5))
-    ax[0, 0].imshow(img_left_rgb)
-    ax[0, 0].set_title('Left RGB Image (cam2)')
+    return pcd
 
-    ax[0, 1].imshow(img_right_rgb)
-    ax[0, 1].set_title('Right RGB Image (cam3)')
+def write_pointcloud(filename,xyz_points,rgb_points=None):
 
-    ax[1, 0].imshow(disparaity_map,cmap='gray')
-    ax[1, 0].set_title('RGB Stereo Disparity')
+    """ creates a .pkl file of the point clouds generated
 
-    ax[1, 1].imshow(depth_map, cmap='flag')
-    ax[1, 1].set_title('RGB Stereo Depth')
+    """
 
-    plt.show()
+    assert xyz_points.shape[1] == 3,'Input XYZ points should be Nx3 float array'
+    if rgb_points is None:
+        rgb_points = np.ones(xyz_points.shape).astype(np.uint8)*255
+    assert xyz_points.shape == rgb_points.shape,'Input RGB colors should be Nx3 float array and have same size as input XYZ points'
+
+    # Write header of .ply file
+    fid = open(filename,'wb')
+    fid.write(bytes('ply\n', 'utf-8'))
+    fid.write(bytes('format binary_little_endian 1.0\n', 'utf-8'))
+    fid.write(bytes('element vertex %d\n'%xyz_points.shape[0], 'utf-8'))
+    fid.write(bytes('property float x\n', 'utf-8'))
+    fid.write(bytes('property float y\n', 'utf-8'))
+    fid.write(bytes('property float z\n', 'utf-8'))
+    fid.write(bytes('property uchar red\n', 'utf-8'))
+    fid.write(bytes('property uchar green\n', 'utf-8'))
+    fid.write(bytes('property uchar blue\n', 'utf-8'))
+    fid.write(bytes('end_header\n', 'utf-8'))
+
+    # Write 3D points to .ply file
+    for i in range(xyz_points.shape[0]):
+        fid.write(bytearray(struct.pack("fffccc",xyz_points[i,0],xyz_points[i,1],xyz_points[i,2],
+                                        rgb_points[i,0].tostring(),rgb_points[i,1].tostring(),
+                                        rgb_points[i,2].tostring())))
+    fid.close()
+
+def visualize(img_left_rgb,img_right_rgb,disparaity_map,depth_map, rgbd = None,withOutRGBD =False):
+    
+    plt.figure(figsize=(20,10))
+
+    plt.subplot(3, 2, 1)
+    plt.title('Left RGB image')
+    plt.imshow(img_left_rgb)
+    plt.colorbar()
+
+
+    plt.subplot(3, 2, 2)
+    plt.title('Right RGB image')
+    plt.imshow(img_right_rgb)
+    plt.colorbar()
+    
+    
+    plt.subplot(3, 2, 3)
+    plt.title('Disparity Map')
+    plt.imshow(disparaity_map)
+    plt.colorbar()
+
+
+    plt.subplot(3, 2, 4)
+    plt.title('Depth Estimation Map')
+    plt.imshow(depth_map)
+    plt.colorbar()
+
+    if withOutRGBD == True: 
+        plt.subplot(3, 2, 5)
+        plt.title('RGBD color Map')
+        plt.imshow(rgbd.color)
+        plt.colorbar()
+            
+        plt.subplot(3, 2, 6)
+        plt.title('RGBD Depth Map')
+        plt.imshow(rgbd.depth)
+        plt.colorbar()
+
+    #plt.show()
+    plt.savefig('3D_scene_images.png')
+
+
+def write_ply(fn, verts, colors):
+    ply_header = '''ply
+    format ascii 1.0
+    element vertex %(vert_num)d
+    property float x
+    property float y
+    property float z
+    property uchar red
+    property uchar green
+    property uchar blue
+    end_header
+    '''
+
+    verts = verts.reshape(-1, 3)
+    colors = colors.reshape(-1, 3)
+    verts = np.hstack([verts, colors])
+    with open(fn, 'wb') as f:
+        f.write((ply_header % dict(vert_num=len(verts))).encode('utf-8'))
+        np.savetxt(f, verts, fmt='%f %f %f %d %d %d ')
+
+
+
+def cv_pointcloud_from_stereo(dataset,frameNumber):
+
+    print('loading images...')
+
+    #imgL = cv2.pyrDown(np.array(dataset.get_cam2(frameNumber)))
+    #imgR = cv2.pyrDown(np.array(dataset.get_cam3(frameNumber)))
+
+    imgL = cv2.pyrDown(cv2.imread(cv2.samples.findFile('left.png')))  # downscale images for faster processing
+    imgR = cv2.pyrDown(cv2.imread(cv2.samples.findFile('right.png')))
+
+
+    # disparity range is tuned for 'aloe' image pair
+    window_size = 3
+    min_disp = 16
+    num_disp = 112-min_disp
+    stereo = cv2.StereoSGBM_create(minDisparity = min_disp,
+        numDisparities = num_disp,
+        blockSize = 16,
+        P1 = 8*3*window_size**2,
+        P2 = 32*3*window_size**2,
+        disp12MaxDiff = 1,
+        uniquenessRatio = 10,
+        speckleWindowSize = 100,
+        speckleRange = 32
+    )
+
+    print('computing disparity...')
+    disp = stereo.compute(imgL, imgR).astype(np.float32) / 16.0
+
+    print('generating 3d point cloud...',)
+    h, w = imgL.shape[:2]
+    
+    f = 0.8*w                          # guess for focal length
+    
+    #f = k_[0, 0]
+
+
+    Q = np.float32([[1, 0, 0, -0.5*w],
+                    [0,-1, 0,  0.5*h], # turn points 180 deg around x-axis,
+                    [0, 0, 0,     -f], # so that y-axis looks up
+                    [0, 0, 1,      0]])
+    
+    points = cv2.reprojectImageTo3D(disp, Q)  # getting depth from disparity map
+    
+    colors = cv2.cvtColor(imgL, cv2.COLOR_BGR2RGB)
+    mask = disp > disp.min()
+    cloudpoint = points[mask]
+    cloudpoint_color = colors[mask]
+
+    print("cloudpoint_data")
+    print(type(cloudpoint))
+    
+    print("cloudpoint_color_data")
+    print(type(cloudpoint_color))
+    
+    
+    #cv2.imshow('left', imgL)
+    #cv2.imshow('right',imgR)
+    #cv2.imshow('disparity', (disp-min_disp)/num_disp)
+    #depth_map = depth_from_disparity((disp-min_disp)/num_disp,None,None,f)
+    #disparity_map = disp
+    #visualize(imgL,imgR,disparity_map,depth_map,None)
+
+    return cloudpoint , cloudpoint_color
